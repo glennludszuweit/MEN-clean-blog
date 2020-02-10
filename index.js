8b1c4ac306c5b45b178e62c9e7e397d8c590e8ba
Glenn Ludszuweit <glenn.ludszuweit@gmail.com>
2020-02-10 03:03:07 +0100

    setup mongoose

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const express = require('express')
-const path = require('path')
-const ejs = require('ejs')
+const express = require('express');
+const app = new express();
+const path = require('path');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
 
-const app = new express()
+mongoose.connect('mongodb://localhost/node-blog', {useNewUrlParser: true})
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.listen(4000, () => {
     console.log('Server running on PORT 4000...');
-})
+});
 
 app.get('/', (req, res) => {
     //res.sendFile(path.resolve(__dirname, 'pages/index.html'))
     res.render('index');
-})
+});
 
 app.get('/about', (req, res) => {
     //res.sendFile(path.resolve(__dirname, 'pages/about.html'))
     res.render('about');
-})
+});
 
 app.get('/contact', (req, res) => {
     //res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
     res.render('contact');
-})
+});
 
 app.get('/post', (req, res) => {
     //res.sendFile(path.resolve(__dirname, 'pages/post.html'))
     res.render('post');
-})
+});
