160352b28f7eb0e49425c1f65d27bf097ca006ea
Glenn Ludszuweit <glenn.ludszuweit@gmail.com>
2020-02-14 00:51:13 +0100

    refactored code to MVC

--- index.js
+++ index.js
@@ -1,63 +1,32 @@
 const express = require("express");
 const app = new express();
-const path = require("path");
-const ejs = require("ejs");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const BlogPost = require("./models/BlogPost.js");
 const fileUpload = require("express-fileupload");
+const path = require("path");
+const ejs = require("ejs");
+const BlogPost = require("./models/BlogPost.js");
+
+const newPostController = require("./controllers/newPost");
+const homeController = require("./controllers/home");
+const getPostController = require("./controllers/getPost");
+const storePostController = require("./controllers/storePost");
+
+const validateMiddleware = require("./middlewares/validationMiddleware");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use("/posts/store", validateMiddleware);
 
-//////Server
 mongoose.connect("mongodb://localhost/node-blog", { useNewUrlParser: true });
 app.listen(4000, () => {
   console.log("Server running on PORT 4000...");
 });
 
-//////Middlewares
-const validateMiddleware = (req, res, next) => {
-  if (req.files == null || req.body.title == null || req.body.title == null) {
-    return res.redirect("/post/new");
-  }
-  next();
-};
-app.use("/posts/store", validateMiddleware);
-
-app.get("/", async (req, res) => {
-  const blogposts = await BlogPost.find({});
-  res.render("index", {
-    blogposts
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
-
-app.get("/post/:id", async (req, res) => {
-  const blogpost = await BlogPost.findById(req.params.id);
-  res.render("post", {
-    blogpost
-  });
-});
-
-app.get("/posts/new", (req, res) => {
-  res.render("create");
-});
-
-app.post("/posts/store", (req, res) => {
-  let image = req.files.image;
-  image.mv(path.resolve(__dirname, "public/img", image.name), async error => {
-    await BlogPost.create({ ...req.body, image: "/img/" + image.name });
-    res.redirect("/");
-  });
-});
+app.get("/", homeController);
+app.get("/post/:id", getPostController);
+app.get("/posts/new", newPostController);
+app.post("/posts/store", storePostController);
