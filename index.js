c27cd38f4aedc9c1bdd9099d741ad338282d0d36
Glenn Ludszuweit <glenn.ludszuweit@gmail.com>
2020-02-16 22:42:01 +0100

    user registration and login

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const path = require("path");
 const ejs = require("ejs");
 const BlogPost = require("./models/BlogPost.js");
 
-const newPostController = require("./controllers/newPost");
 const homeController = require("./controllers/home");
+const newPostController = require("./controllers/newPost");
 const getPostController = require("./controllers/getPost");
 const storePostController = require("./controllers/storePost");
+const newUserController = require("./controllers/newUser");
+const storeUserController = require("./controllers/storeUser");
+const loginController = require("./controllers/login");
+const loginUserController = require("./controllers/loginUser");
 
-const validateMiddleware = require("./middlewares/validationMiddleware");
+const validateMiddleware = require("./middlewares/validateMiddleware");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -30,3 +34,7 @@ app.get("/", homeController);
 app.get("/post/:id", getPostController);
 app.get("/posts/new", newPostController);
 app.post("/posts/store", storePostController);
+app.get("/auth/register", newUserController);
+app.post("/users/register", storeUserController);
+app.get("/auth/login", loginController);
+app.post("/users/login", loginUserController);
