97c2a659132a4df8d9c13e77ae985369c1074f23
Glenn Ludszuweit <glenn.ludszuweit@gmail.com>
2020-02-19 12:32:27 +0100

    Authenticate user with express-session

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fileUpload = require("express-fileupload");
 const path = require("path");
 const ejs = require("ejs");
 const BlogPost = require("./models/BlogPost.js");
+const expressSession = require("express-session");
 
 const homeController = require("./controllers/home");
 const newPostController = require("./controllers/newPost");
@@ -17,13 +18,26 @@ const loginController = require("./controllers/login");
 const loginUserController = require("./controllers/loginUser");
 
 const validateMiddleware = require("./middlewares/validateMiddleware");
+const authMiddleware = require("./middlewares/authMiddleware");
+const redirectIfAuthMiddleware = require("./middlewares/redirectIfAuthMiddleware");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(
+  expressSession({
+    secret: "keyboard cat"
+  })
+);
 app.use("/posts/store", validateMiddleware);
+
+global.loggedIn = null;
+app.use("*", (req, res, next) => {
+  loggedIn = req.session.userId;
+  next();
+});
 
 mongoose.connect("mongodb://localhost/node-blog", { useNewUrlParser: true });
 app.listen(4000, () => {
@@ -32,9 +46,9 @@ app.listen(4000, () => {
 
 app.get("/", homeController);
 app.get("/post/:id", getPostController);
-app.get("/posts/new", newPostController);
-app.post("/posts/store", storePostController);
-app.get("/auth/register", newUserController);
-app.post("/users/register", storeUserController);
-app.get("/auth/login", loginController);
-app.post("/users/login", loginUserController);
+app.get("/posts/new", authMiddleware, newPostController);
+app.post("/posts/store", authMiddleware, storePostController);
+app.get("/auth/register", redirectIfAuthMiddleware, newUserController);
+app.post("/users/register", redirectIfAuthMiddleware, storeUserController);
+app.get("/auth/login", redirectIfAuthMiddleware, loginController);
+app.post("/users/login", redirectIfAuthMiddleware, loginUserController);
