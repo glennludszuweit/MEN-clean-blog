c9ee623b9ea88a616f3538e7b7d6915037e0c109
Glenn Ludszuweit <48032151+glennludszuweit@users.noreply.github.com>
2020-02-24 18:59:22 +0100

    added missing controller folder

---
+++ controllers/newPost.js
@@ -0,0 +1,26 @@
+module.exports = (req, res) => {
+  if (req.session.userId) {
+    let imageError = false;
+    let titleError = false;
+    let descriptionError = false;
+
+    if (typeof req.flash("titleError")[0] != "undefined") {
+      titleError = true;
+    }
+
+    if (typeof req.flash("descriptionError")[0] != "undefined") {
+      descriptionError = true;
+    }
+
+    if (typeof req.flash("imageError")[0] != "undefined") {
+      imageError = true;
+    }
+
+    return res.render("create", {
+      titleError: titleError,
+      descriptionError: descriptionError,
+      imageError: imageError
+    });
+  }
+  res.redirect("/auth/login");
+};
---
+++ controllers/newUser.js
@@ -0,0 +1,19 @@
+module.exports = (req, res) => {
+  var username = "";
+  var email = "";
+  var password = "";
+  const data = req.flash("data")[0];
+
+  if (typeof data != "undefined") {
+    username = data.username;
+    email = data.email;
+    password = data.password;
+  }
+
+  res.render("register", {
+    errors: req.flash("validationErrors"),
+    username: username,
+    email: email,
+    password: password
+  });
+};
